3eeb4d8bb4fbe3021d82dc87fb650779bc04eb03
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-01-09 22:30:37 -0800

    My Thread Task

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        BufferedReader reader = new BufferedReader(new FileReader("text.txt"));
+        String line;
+        FutureTask[] returnedTask = new FutureTask[5];
+
+        while((line = reader.readLine()) != null){
+
+            Callable callable = new MyCallable();
+            final String tempLine = line;
+            Thread t =  Thread.currentThread().getName() + "-" + tempLine.split(" ").length);
+
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+            });
+
+        }
+    }
+}
---
+++ src/MyCallable.java
@@ -0,0 +1,12 @@
+import java.util.concurrent.Callable;
+
+class MyCallable implements Callable {
+
+   @Override
+   public Object call() throws Exception {
+
+      int sumNumber = 0;
+      return sumNumber;
+
+   }
+}
